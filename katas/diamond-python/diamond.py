454ef2f2bf4d437fe5a4d75f2daa29fae8cda4c0
Håkan Wiklund <hakan.wiklund@gmail.com>
2014-02-16 21:56:25 +0100

    done, tried to reduce the number of rows. :-)

--- katas/diamond-python/diamond.py
+++ katas/diamond-python/diamond.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-import string
+import string, re
 
 def printDiamond(stop):
-    idx = ord(stop) - ord('A')
-    full = string.ascii_uppercase[idx:1:-1] + string.ascii_uppercase[0:idx+1]  
-    print full
-
+    full = string.ascii_uppercase[(ord(stop) - ord('A')):0:-1] + string.ascii_uppercase[0:(ord(stop) - ord('A'))+1]  
+    for i in xrange(0, len(full)):
+        print re.sub("[^" + chr(ord('A') + (i if i < len(full)/2 else len(full) - i -1)) + "]", " " , full)
+        
 printDiamond('F')
