b7963041eb0b64e641766b768495b1ff13d4393f
Håkan Wiklund <hakan.wiklund@gmail.com>
2014-03-12 21:21:39 +0100

    primtal groovy

---
+++ katas/primtal/src/prime1.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/groovy
+
+def sievePrimes = { bound -> 
+    def isPrime  = new BitSet(bound)
+    isPrime[0..1] = false
+    isPrime[2..<bound] = true
+    (2..(Math.sqrt(bound))).each { pc ->
+        if (isPrime[pc]) {
+            ((pc**2)..<bound).step(pc) { isPrime[it] = false }
+        }
+    }
+    (0..<bound).findAll { isPrime[it] }
+}
+
+
+println sievePrimes(args[0].toInteger())
